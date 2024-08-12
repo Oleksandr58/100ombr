dc841f01c16a006b6a8eaaf296fcaad8726d3de3
Oleksandr Kovaliuk <o.kovaliuk@steel.mil.ua>
2024-08-12 10:38:32 +0300

    Add priority

--- main.js
+++ main.js
@@ -1,48 +1,95 @@
-const selectList = document.querySelectorAll(".data-priority");
+const priorityLabel = document.querySelector(".data-priority-label");
 
-selectList.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    e.currentTarget.parentElement.classList.toggle(
-      "wonder_apps__input_select--active"
-    );
-  });
+sortByPriority();
+
+priorityLabel.addEventListener("click", (e) => {
+  if (
+    e.currentTarget.classList.value
+      .split(" ")
+      .includes("data-priority-label--reverse") ||
+    e.currentTarget.classList.value
+      .split(" ")
+      .includes("data-priority-label--nofilter")
+  ) {
+    e.currentTarget.classList = "data-priority-label";
+    e.currentTarget.innerHTML = "По пріоритету &#8595;";
+    sortByPriority();
+  } else {
+    e.currentTarget.classList =
+      "data-priority-label data-priority-label--reverse";
+    e.currentTarget.innerHTML = "По пріоритету &#8593;";
+    sortByPriority(true);
+  }
 });
 
-const optionList = document.querySelectorAll(".wonder_apps__input_select_item");
+function sortByPriority(isReverse) {
+  const selectList = document.querySelectorAll(".data-priority");
+  const selectListPriority = [];
 
-optionList.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    e.currentTarget.parentElement.parentElement.querySelector(
-      ".wonder_apps__input_select"
-    ).textContent = e.currentTarget.textContent;
-    e.currentTarget.parentElement.parentElement.setAttribute(
-      "data-value",
-      e.currentTarget.textContent
-    );
-    e.currentTarget.parentElement.parentElement.classList.toggle(
-      "wonder_apps__input_select--active"
-    );
+  selectList.forEach((element) => {
+    selectListPriority.push({ priority: element.textContent, element });
   });
+  selectListPriority.sort((a, b) =>
+    isReverse ? b.priority - a.priority : a.priority - b.priority
+  );
+
+  const wrapper = document.querySelector(".wonder_apps_cards");
+  wrapper.innerHTML = "";
+  selectListPriority.forEach((item) => {
+    wrapper.append(item.element.parentElement.parentElement.parentElement);
+  });
+  resetPrice();
+}
+function resetPriority() {
+  const selectList = document.querySelector(".data-priority-label");
+
+  selectList.classList = "data-priority-label data-priority-label--nofilter";
+  selectList.innerHTML = "По пріоритету";
+}
+
+const priceLabel = document.querySelector(".data-price-label");
+
+priceLabel.addEventListener("click", (e) => {
+  if (
+    e.currentTarget.classList.value
+      .split(" ")
+      .includes("data-price-label--reverse") ||
+    e.currentTarget.classList.value
+      .split(" ")
+      .includes("data-price-label--nofilter")
+  ) {
+    e.currentTarget.classList = "data-price-label";
+    e.currentTarget.innerHTML = "По ціні &#8595;";
+    sortByPrice();
+  } else {
+    e.currentTarget.classList = "data-price-label data-price-label--reverse";
+    e.currentTarget.innerHTML = "По ціні &#8593;";
+    sortByPrice(true);
+  }
 });
 
-const menu = document.querySelectorAll(".wonder_apps_header_menu");
+function sortByPrice(isReverse) {
+  const selectList = document.querySelectorAll(".data-price");
+  const selectListPriority = [];
 
-menu.forEach((element) => {
-  element.addEventListener("click", () => {
-    console.log("click");
-    document
-      .querySelector(".wonder_apps__header_nav")
-      .classList.toggle("wonder_apps__header_nav--active");
+  selectList.forEach((element, index) => {
+    selectListPriority.push({ priority: element.textContent, element });
   });
-});
+  selectListPriority.sort((a, b) =>
+    isReverse ? b.priority - a.priority : a.priority - b.priority
+  );
 
-const menuItems = document.querySelectorAll(".wonder_apps__header_nav ul li");
-
-menuItems.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    console.log(e.currentTarget.parentElement);
-    e.currentTarget.parentElement.parentElement.classList.toggle(
-      "wonder_apps__header_nav--active"
-    );
+  const wrapper = document.querySelector(".wonder_apps_cards");
+  wrapper.innerHTML = "";
+  selectListPriority.forEach((item) => {
+    wrapper.append(item.element.parentElement.parentElement.parentElement);
   });
-});
+  resetPriority();
+}
+
+function resetPrice() {
+  const selectList = document.querySelector(".data-price-label");
+
+  selectList.classList = "data-price-label data-price-label--nofilter";
+  selectList.innerHTML = "По ціні";
+}
